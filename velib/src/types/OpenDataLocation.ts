16e7e7f2900a739504a9d749a643ee011783af70
Abdouni Abdelkarim <abdounikarim@gmail.com>
2024-10-30 22:40:16 +0100

    OpenDataLocation - remove unused fields

--- velib/src/types/OpenDataLocation.ts
+++ velib/src/types/OpenDataLocation.ts
@@ -2,11 +2,8 @@ export class OpenDataLocation {
     public address?: string;
     public available_bike_stands?: number;
     public available_bikes?: number;
-    public banking?: boolean;
     public bike_stands?: number;
     public bonus?: boolean;
-    public contract_name?: string;
-    public last_update?: number;
     public name?: string;
     public number?: number;
     public position?: {
