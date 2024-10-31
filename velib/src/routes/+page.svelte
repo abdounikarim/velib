a1f8c6351cd55d54ffea806a08edcc3697009d44
Abdouni Abdelkarim <abdounikarim@gmail.com>
2024-10-31 22:37:34 +0100

    Create Map component and use it

--- velib/src/routes/+page.svelte
+++ velib/src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import * as GMaps from "@googlemaps/js-api-loader";
-    const { Loader } = GMaps;
-    import { PUBLIC_GOOGLEMAPS_API_KEY } from '$env/static/public';
-    import Marker from "../components/Marker";
     import OpenData from "../components/OpenData";
+    import Map from "../components/Map";
 
     // Bindings
     let mapElement: HTMLElement;
@@ -13,21 +10,8 @@
         const openData = new OpenData();
         const openDataStations = await openData.getData();
 
-        const loader = new Loader({
-            apiKey: PUBLIC_GOOGLEMAPS_API_KEY,
-            version: 'weekly'
-        });
-
-        const { Map } = await loader.importLibrary('maps');
-
-        let map = new Map(mapElement, {
-            center: { lat: 45.7621258, lng: 4.8576067 },
-            mapId: "VELIB_MAP_ID",
-            zoom: 13
-        });
-
-        const marker = new Marker();
-        marker.initMarkers(map, openDataStations);
+        const map = new Map(mapElement);
+        await map.addMarkers(openDataStations);
     });
 </script>
 
