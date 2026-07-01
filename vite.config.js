a8ed9dab2b866351f4567da2845369b566cb3191
Abdouni Abdelkarim <abdounikarim@gmail.com>
2026-07-02 01:54:33 +0200

    build: add vite config

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import fs from 'fs'
+
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+
+  const serverConfig = {
+    host: true,
+    port: 5173,
+  }
+
+  if (isDev && fs.existsSync('certs/tls.key') && fs.existsSync('certs/tls.pem')) {
+    serverConfig.https = {
+      key:  fs.readFileSync('certs/tls.key'),
+      cert: fs.readFileSync('certs/tls.pem'),
+    }
+  }
+
+  return {
+    server: serverConfig,
+    preview: {
+      port: 4173,
+    },
+  }
+})
