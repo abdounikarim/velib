373f59971590e63b42b9ab6946e456540020bf2f
Abdouni Abdelkarim <abdounikarim@gmail.com>
2026-07-02 02:16:54 +0200

    fix: Uncaught ReferenceError: MarkerClusterer is not defined

--- js/VelibMarkerCluster.js
+++ js/VelibMarkerCluster.js
@@ -1,7 +1,8 @@
 var VelibMarkerCluster = {
     init: function() {
-        var markerCluster = new MarkerClusterer(VelibMap.map, VelibMarker.locations, {
-            imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        var markerCluster = new markerClusterer.MarkerClusterer({
+            map: VelibMap.map,
+            markers: VelibMarker.locations,
         });
 
         return markerCluster;
